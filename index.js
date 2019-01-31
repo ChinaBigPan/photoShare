8ceda0e60f02157c7bf23d5d646225718afa42b5
GuoXiaopeng <xiaopeng15@staff.sina.com.cn>
2019-01-31 10:01:16 +0800

    提交提交

--- index.js
+++ index.js
@@ -1,75 +1,31 @@
-// 1. 引入 "apollo-server"
-const { ApolloServer } = require('apollo-server');
 
-const typeDefs =`
-    enum PhotoCategory {
-        SELFIE
-        PORTRAIT
-        ACTION
-        LANDSCAPE
-        GRAPHIC
-    }
-    type Photo {
-        id: ID!
-        url: String!
-        name: String!
-        description: PhotoCategory
-    }
-    type Query {
-        totalPhotos: Int!
-        allPhotos: [Photo!]!
-    }
-    input PostPhotoInput {
-        name: String!
-        category: PhotoCategory = PORTRAIT
-        description: String
-    }
-    type Mutation {
-        postPhoto(input: PostPhotoInput!): Photo!
-    }
-`;
-// 1. 设置一个自增变量以绑定ID
-var _id = 0;
-var photos = [];
-const resolvers = {
-    Query: {
-        totalPhotos: () => photos.length,
-        allPhotos: () => photos
-    },
-    //  变更（mutation）和postPhot解析器
-    Mutation: {
-        postPhoto(parent, args) {
-            // 2. 创建一个带有id的新对象
-            var newPhoto = {
-                id: _id++,
-                ...args
-            };
-            photos.push(newPhoto);
-            // 3. 返回新的照片类型
-            return newPhoto;
-        }
-    }
-};
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const expressPlayground = require("graphql-playground-middleware-express")
+    .default;
+const { readFileSync } = require("fs");
+const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
+const resolvers = require("./resolvers");
 
-// 2. 创建一个新的服务器实例
-// 3. 并将typeDefs (schema)和接卸气作为对象参数传入
+const app = express();
+
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
+server.applyMiddleware({ app });
 
-// 调用listen() 启动服务器
-server
-    .listen()
-    .then(({ url }) => console.log(`GraphQL服务运行于${ url }`));
+app.get("/", (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // 设置utf-8
+    res.end("欢迎来到我们的照片分享API");
+});
 
+app.get("/playground", expressPlayground({
+    endpoint: "/graphql"
+}));
 
-
-
-
-
-
-
-
-
+const port = 4000;
+app.listen({ port }, () => {
+    console.log(`GraphQL服务运行于http://localhost:${port}, 路径为${server.graphqlPath}`);
+});
